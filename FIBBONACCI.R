142b4addc067dc234e2d32273dbaae2ca454c937
Beto <beto.marin@gmail.com>
2018-11-15 12:03:19 +0100

    PRIMER PASO

---
+++ FIBBONACCI.R
@@ -0,0 +1,13 @@
+#TASK PARA DEFINIR UNA FUNCION QUE CALCULE LA SERIE FIBONACCI 
+
+for(i in 1:(ncol(survey))) {    #for every column
+  if (is.numeric(survey[,i])){  #if the variable is numeric
+    qqnorm(survey[,i],main=paste("Normal Q-Q Plot of",colnames(survey)[i])) #plot qqnorm
+    qqline(survey[,i],col="red") #add qqnormal line in red
+    hist(survey[,i],main=paste("Histogram of",colnames(survey)[i]), #make the histogram
+         xlab=colnames(survey)[i])
+  }
+}
+
+for (RN in c(R1:R15))
+  
